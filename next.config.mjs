8d6792d12c7908bc285ea276bb4c02034bbf41b6
Purna Mummani <purnasmac@Purnas-MacBook-Air.local>
2026-01-16 19:41:34 -0500

    fix for workflow

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // These lines are critical to ignore the errors preventing your build
   typescript: {
     ignoreBuildErrors: true,
   },
