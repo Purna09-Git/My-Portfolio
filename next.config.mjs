ffb114ee56525ed9804be33146d3495fbb2a5f4f
Purna Mummani <purnasmac@Purnas-MacBook-Air.local>
2026-01-16 19:44:33 -0500

    add basepath for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  // Replace 'My-Portfolio' with your exact GitHub repository name
+  basePath: '/My-Portfolio', 
+  assetPrefix: '/My-Portfolio',
   images: {
     unoptimized: true,
   },
